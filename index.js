49feb37c647e57ea6d0c7bfd3b98d7906ea67ed0
RehmahMuneeb <rehmahbaber47@gmail.com>
2024-03-19 16:17:45 +0500

    calculator

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-let input = document.getElementById('inputbox');
+// let input = document.getElementById('inputbox');
+// let buttons = document.querySelectorAll('button');
+// let string = "";
+// let arr = Array.from(buttons)
+// arr.forEach(button =>{
+//     button.addEventListener('click' ,(e) =>{
+//     if(e.target.innerHTML === '='){
+//         string = eval(string);
+//         input.value = string
+//     }else if(e.target.innerHTML === 'AC'){
+//         string = "";
+//         input.value = string;
+//     }else if(e.target.innerHTML === 'DEL'){
+//         string = string.substring(0 , string.length-1);
+//         input.value = string;
+//     }else{
+//         string += e.target.innerHTML;
+//         input.value = string;
+//     }
+//     })
+// })
+let input = document.querySelector('#inputbox');
 let buttons = document.querySelectorAll('button');
 let string = "";
-// let arr = Array.from(buttons);
-// arr.forEach(button =>{
-//     button.addEventListener('click' , (e) =>{
-//         if(e.target.innerHTML === '='){
-//             string = eval(string);
-//             input.value = string;
-//         }
-//         // else if(e.target.innerHTML = 'AC'){
-//         //     string = "";
-//         //     input.value = string;
-//         //     }
-//             // else if(e.target.innerHTML = 'DEL'){
-//             //     string = string.substring(0, string.length-1)
-//             //     input.value = string;
-//             // } 
-//             else{
-//                 string += e.target.innerHTML;
-//                 input.value = string;
-//             }
-//         })
-//     });
-let arr = Array.from(buttons)
+let arr = Array.from(buttons);
 arr.forEach(button =>{
-    button.addEventListener('click' ,(e) =>{
-    if(e.target.innerHTML == '='){
+    button.addEventListener('click', (e) =>{
+      if(e.target.innerHTML === '='){
         string = eval(string);
-        input.value = string
-    }else if(e.target.innerHTML == 'AC'){
+        input.value = string;
+      }else if(e.target.innerHTML === 'DEL'){
+        string = string.substring(0 , string.length-1)
+            input.value = string;
+      }else if(e.target.innerHTML === 'AC'){
         string = "";
         input.value = string;
-    }else if(e.target.innerHTML = 'DEL'){
-        string = string.substring(0 , string.length-1);
-        input.value = string;
-    }else{
+      }else{
         string += e.target.innerHTML;
-        input.value = string;
-    }
+        input.value = string
+      }
     })
 })
